10cc75f2a8bac124fac76c468bce790b6fae7fde
arjava <arjava379@gmail.com>
2022-08-19 13:21:48 +0700

    update setting port embedded server

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,10 +2,17 @@ package com.example
 
 import io.ktor.server.application.*
 import com.example.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.ktor.server.plugins.methodoverride.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(XHttpMethodOverride)
+        configureSerialization()
+        configureRouting()
+    }.start(wait = true)
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
